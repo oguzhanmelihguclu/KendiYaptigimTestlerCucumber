197802b587657128c978b161517dc454655ee30a
oguzhanmelihguclu <oguzhanmguclu@gmail.com>
2025-04-15 18:29:56 +0300

    oguz

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,7 +7,7 @@ import org.junit.platform.suite.api.ConfigurationParameter;
 
 @Cucumber
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features") // features klasörü yolunu doğru belirttiğinden emin ol
+@SelectClasspathResource("src/test/resources/features") // features klasörü yolunu doğru belirttiğinden emin ol
 @ConfigurationParameter(key = "cucumber.plugin", value = "json:target/cucumber.json") // JSON raporu oluşturmak için
 @ConfigurationParameter(key = "cucumber.glue", value = "stepdefinitions") // Adım tanımlarının yerini belirt
 public class Runner {
